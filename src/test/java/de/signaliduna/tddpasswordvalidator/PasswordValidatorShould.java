0e1be0b3538281147803bb5b2e6233d24d411297
Jannik Reinefeld <jannik.reinefeld@gmail.com>
2022-01-25 10:06:30 +0100

    Added comments in english as well

--- src/test/java/de/signaliduna/tddpasswordvalidator/PasswordValidatorShould.java
+++ src/test/java/de/signaliduna/tddpasswordvalidator/PasswordValidatorShould.java
@@ -11,6 +11,14 @@ import org.junit.Test;
  * - mindestens 3 Zahlen
  *
  * Bitte nutzt den Aufruf: PasswordValidator.validate("Passwort");
+ *
+ * ----------------------------
+ * Assignment: Create a password validator using TDD
+ * Create a static method which you hand over a password and which will return "secure" or "not secure"
+ * A secure password contains
+ * - at least 12 characters
+ * - at least 1 special character (Special characters for this example are are % & ! ? &)
+ * - at least 3 numbers
  */
 public class PasswordValidatorShould {
 
